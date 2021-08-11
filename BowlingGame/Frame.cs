23c97874d2627e458bc531e58914755e59259595
Nicholas Piazza <nickajp@comcast.net>
2021-08-11 12:24:03 -0500

    1. In Frame.cs  a. Changed Score from readonly to read/write.  b. Added method Add() 2. In FrameTest.cs  a. Added TestMethod TestAddOneThrow()

--- BowlingGame/Frame.cs
+++ BowlingGame/Frame.cs
@@ -8,6 +8,11 @@ namespace BowlingGame
 {
     public class Frame
     {
-        public int Score { get; }
+        public int Score { get; set; }
+
+        public void Add (int pins)
+        {
+            Score += pins;
+        }
     }
 }
--- FrameTest/FrameTest.cs
+++ FrameTest/FrameTest.cs
@@ -13,5 +13,14 @@ namespace FrameTest
 
             Assert.AreEqual (0, frame.Score);
         }
+
+        [TestMethod]
+        public void TestAddOneThrow ()
+        {
+            Frame frame = new Frame ();
+
+            frame.Add (5);
+            Assert.AreEqual (5, frame.Score);
+        }
     }
 }
