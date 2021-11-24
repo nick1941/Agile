97f768aebd5123bfdda1dd738f548fe911ebfb41
Nicholas Piazza <nickajp@comcast.net>
2021-11-24 15:49:37 -0600

    1. In Game.cs  a. Made 'ball', 'firstThrow', and 'secondThrow' member variables  b. Added private properties NextBall, NextTwoBalls, TwoBallsInFrame  c. Added private methods Spare(), Strike()  d. In HandleSecondThrow(), refactored to use NextBall, TwoBallsInFrame, and Spare and eliminated need for frameScore  e. In ScoreForFrame(), refactored to use Strike(), NextTwoBalls, and HandleSecondThrow() 2. Updated three project files to .NET 6

--- Agile/BowlingGame/Game.cs
+++ Agile/BowlingGame/Game.cs
@@ -14,12 +14,35 @@ namespace BowlingGame
 
         #region Fields
         private bool   isFirstThrow = true;
+        private int    ball;
         private int    currentFrame = 1;
         private int    currentThrow;
+        private int    firstThrow;
+        private int    secondThrow;
         private int [] throws = new int [MAX_THROWS_IN_GAME];
         #endregion Fields
 
         #region Properties
+        // Get a count of the next ball thrown
+        private int NextBall
+        {
+            get { return throws [ball]; }
+        }
+
+        // Get a count of the next two balls thrown.
+        private int NextTwoBalls
+        {
+            get { return (throws [ball] + throws [ball + 1]); }
+        }
+
+        // Get a count from two balls in the frame.
+        private int TwoBallsInFrame
+        {
+            get { return throws [ball] + throws [ball + 1]; }
+        }
+        /// <summary>
+        /// Get the score for the specified frame.
+        /// </summary>
         public int Score 
         { 
             get 
@@ -28,6 +51,9 @@ namespace BowlingGame
             { }
         }
 
+        /// <summary>
+        /// Get the current frame number.
+        /// </summary>
         public int CurrentFrame
         {
             get { return currentFrame; }
@@ -35,6 +61,9 @@ namespace BowlingGame
         #endregion Properties
 
         #region PrivateMethods
+        // Adjust the current frame number based on the number of pins.
+        // Parameters:
+        //  pins:   Number of pins knocked down
         private void AdjustCurrentFrame (int pins)
         {
             if (isFirstThrow)
@@ -52,6 +81,42 @@ namespace BowlingGame
             if (currentFrame > 11)
                 currentFrame = 11;
         }
+
+        // Handle the second throw by adjusting the score.
+        private int HandleSecondThrow ()
+        {
+            int score = 0;
+
+            secondThrow = throws[ball + 1];
+
+            // spare needs next frame's first throw
+            if (Spare ())
+            {
+                ball  += 2;
+                score += 10 + NextBall;
+            }
+            else
+            {
+                score += TwoBallsInFrame;
+                ball  += 2;
+            }
+            return score;
+        }
+
+        // Is this a spare?
+        // returns true if a spare; false if not
+        private bool Spare ()
+        {
+            return throws [ball] + throws [ball + 1] == 10;
+        }
+
+        // Is this a strike?
+        // returns true if a strike; false if not
+        private bool Strike ()
+        {
+            return throws [ball] == 10;
+        }
+
         #endregion PrivateMethods
 
         #region PublicMethods
@@ -76,9 +141,10 @@ namespace BowlingGame
         /// <returns>The score for the specified frame number</returns>
         public int ScoreForFrame (int theFrame)
         {
-            int ball  = 0;
             int score = 0;
-            
+
+            ball = 0;
+
             // Initialize the current frame to 0;
             // while the current frame is less than the passed-in frame,
             // compute the score
@@ -88,22 +154,16 @@ namespace BowlingGame
                  currentFrame < theFrame;
                  currentFrame++)
             {
-                int firstThrow  = throws [ball++];
+                firstThrow  = throws [ball];
 
-                if (firstThrow == 10) // strike
+                if (Strike ())
                 {
-                    score += 10 + throws [ball] + throws [ball + 1];
+                    ball++;
+                    score += 10 + NextTwoBalls;
                 }
                 else
                 {
-                    int secondThrow = throws [ball++];
-                    int frameScore = firstThrow + secondThrow;
-
-                    // spare needs next frame's first throw
-                    if (frameScore == 10)
-                        score += frameScore + throws [ball];
-                    else
-                        score += frameScore;
+                    score += HandleSecondThrow ();
                 }
 
             } // method ScoreForFrame
